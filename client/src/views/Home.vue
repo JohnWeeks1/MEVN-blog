5fd36d6c62dc1c3a525316d419708685039470bb
JohnWeeks1 <jonathanweeks007@gmail.com>
2020-11-22 20:25:05 +0000

    first commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+    <p>{{ posts }}</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      posts: null,
+    };
+  },
+  mounted() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      try {
+        this.posts = await axios.get('http://localhost:4001/api/posts');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
