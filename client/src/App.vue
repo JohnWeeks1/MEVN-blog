9988af4b7e574d261be3ddd2ee69f0c27179af35
JohnWeeks1 <jonathanweeks007@gmail.com>
2020-11-22 20:35:54 +0000

    First commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <h1>Hello</h1>
+    <p>{{ posts }}</p>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      posts: null,
+    };
+  },
+  mounted() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      try {
+        this.posts = await axios.get('http://localhost:4001/api/posts');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
